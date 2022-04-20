7fcaa11bc4182f7a01301b283f169c6fe7edff30
miguelangellus <miguelangellus13@outlook.com>
2022-04-20 01:22:48 -0300

    tweak listening port

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	log.Println("\n1.server is starting...\n2.follow: http://localhost:8089\n3.hint: it only can be accessed by binding a port, so run..\ndocker run --rm -d -p 8089:8089 go-static-site")
+	log.Println("\n1.server is starting...\n2.follow: http://localhost:8080\n3.hint: it only can be accessed by binding a port, so run..\ndocker run --rm -d -p 8089:8089 go-static-site")
 
-	log.Fatal(http.ListenAndServe(":8089", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	//optional
 	//err := http.ListenAndServe(":8089", nil)
